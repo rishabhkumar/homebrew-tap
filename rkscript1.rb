07aa7cf4896628005519d3ecbe1186c127821e8b
rishabhkumar <rishabh.jarvis@gmail.com>
2016-02-19 00:46:05 +0530

    yo

---
+++ rkscript1.rb
@@ -0,0 +1,39 @@
+
+
+
+
+class Rkscript1 < Formula
+
+  homepage ""
+  url "https://github.com/rishabhkumar/rkscript1/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "5f21c5cba06b11c7d38c5e7f080d4c05ec6947f0f54d351204fc97496b469a80"
+
+ 
+
+
+  def install
+  bin.install "rkscript1"
+
+  
+
+
+
+
+
+
+
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+  end
