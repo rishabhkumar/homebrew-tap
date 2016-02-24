8562daeca577453c9282398c5f5ef4114cd42525
rishabhkumar <rishabh.jarvis@gmail.com>
2016-02-25 03:09:26 +0530

    h4

---
+++ dictionary.rb
@@ -0,0 +1,10 @@
+class Dictionary < Formula
+  homepage ""
+  url "https://github.com/rishabhkumar/dictionary/archive/1.0.0.zip"
+  version "1.0.0"
+  sha256 "85d2a679178f0e8b796a4c4a9f61313bb79689ce5f08da651191a2edfb677699"
+
+  def install
+ 	bin.install "dictionary"
+  end
+end
